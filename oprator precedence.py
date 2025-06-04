878242545a4ca3ad8b8169c814c990d08efc31d4
ShauryaSuhane <suhaneshaurya26@gmail.com>
2025-06-04 10:06:08 +0530

    operator precedence

---
+++ oprator precedence.py
@@ -0,0 +1,14 @@
+a=4
+b=7
+c=2
+d=12
+e=9
+equation=(a*b)+(d//c)-e
+print("the answer of (a*b)+(d//c)-e is:", equation)
+
+name="henry"
+age=1
+if name=="henry" or name=="john" and age>=2:
+    print("he can eat") 
+else:
+    print("he cannot eat yet")
