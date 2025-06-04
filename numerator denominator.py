dca67c03cc627b5401c61515a1997fc2e944ff64
ShauryaSuhane <suhaneshaurya26@gmail.com>
2025-06-04 10:14:44 +0530

    numerator denominator

---
+++ numerator denominator.py
@@ -0,0 +1,7 @@
+
+numerator=int(input("enter the number:"))
+denominator=int(input("enter the number:"))
+if numerator%denominator==0:
+    print("numerator and denominator are divisible")
+else:
+    print("numerator and denominator are not divisible")
